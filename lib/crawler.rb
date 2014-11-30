964a0bb9bd5082d04f529362339b971ed3613f7a
yoni <johnny.meetme@gmail.com>
2014-11-30 23:34:50 +0200

    Crawler ready

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -4,16 +4,20 @@ Dotenv.load
 
 require_relative '../models/init'
 
-browser = Watir::Browser.new
+client = Selenium::WebDriver::Remote::Http::Default.new
+client.timeout = 300 # seconds – default is 60
+
+browser = Watir::Browser.new :firefox, :http_client => client
+
 browser.goto 'http://www.knesset.gov.il/lobbyist/heb/lobbyist.aspx'
 
-browser.links(:xpath, '//a[@lobbyist_id]')[0..1].map(&:id).each do |id|
-  link = browser.link(id: id)
-  link.click
-  # puts link.attribute_value 'id'
-  lobbyist_web_id = link.attribute_value 'lobbyist_id'
-  Watir::Wait.until { browser.text.include? 'פרטי השדלן:' }  
+links_ids = browser.links(:xpath, '//a[@lobbyist_id]').map{|link| [link.attribute_value(:id), link.attribute_value(:lobbyist_id)]}
 
+links_ids.each do |link_id|
+  browser.goto 'http://www.knesset.gov.il/lobbyist/heb/lobbyist.aspx'  
+  sleep 1
+  link = browser.a(:id, link_id[0])
+  link.click  
   spans = browser.spans(:class => 'text3')
   lobbyist_name = spans[0].text
   lobbying_company_name = spans[1].text
@@ -24,7 +28,7 @@ browser.links(:xpath, '//a[@lobbyist_id]')[0..1].map(&:id).each do |id|
   lobbying_company.update(:corp_id => lobbying_company_corp_id) unless lobbying_company_corp_id.nil? || lobbying_company_corp_id.empty?
 
   lobbyist = Lobbyist.find_or_create(:name => lobbyist_name, :lobbying_company_id => lobbying_company.id)
-  lobbyist.update(:web_id => lobbyist_web_id) unless lobbyist_web_id.nil? || lobbyist_web_id.empty?
+  lobbyist.update(:web_id => link_id[1])
 
   company_names.each do |company_name|
     company = Company.find_or_create(:name => company_name)
@@ -35,7 +39,52 @@ browser.links(:xpath, '//a[@lobbyist_id]')[0..1].map(&:id).each do |id|
   puts "Company #{lobbying_company_name}"
   company_names.each{|c| puts "Lobbying for: #{c}" }
   puts
-  puts  
+  puts    
+end
+
+browser.close
+exit
+
+browser.links(:xpath, '//a[@lobbyist_id]').map(&:id).each do |id|
+  link = browser.link(id: id)
+  link.click
+  # puts link.attribute_value 'id'
+  
+  lobbyist_web_id = begin
+    link.attribute_value 'lobbyist_id'  
+  rescue 
+    puts 'Failed to get lobbyist_id'
+  end  
+
+  begin
+    Watir::Wait.until { browser.text.include? 'פרטי השדלן:' }    
+
+    spans = browser.spans(:class => 'text3')
+    lobbyist_name = spans[0].text
+    lobbying_company_name = spans[1].text
+    lobbying_company_corp_id = spans[2].text
+    company_names = spans[4..spans.size - 1].map{|span| span.text}
+
+    lobbying_company = LobbyingCompany.find_or_create(:name => lobbying_company_name)
+    lobbying_company.update(:corp_id => lobbying_company_corp_id) unless lobbying_company_corp_id.nil? || lobbying_company_corp_id.empty?
+
+    lobbyist = Lobbyist.find_or_create(:name => lobbyist_name, :lobbying_company_id => lobbying_company.id)
+    lobbyist.update(:web_id => lobbyist_web_id) unless lobbyist_web_id.nil? || lobbyist_web_id.empty?
+
+    company_names.each do |company_name|
+      company = Company.find_or_create(:name => company_name)
+      DB.run "INSERT INTO companies_lobbyists VALUES(default, #{company.id}, #{lobbyist.id}, NOW(), NOW())" 
+    end
+
+    puts "Name #{lobbyist_name}"
+    puts "Company #{lobbying_company_name}"
+    company_names.each{|c| puts "Lobbying for: #{c}" }
+    puts
+    puts    
+  rescue Exception => e
+    puts "!!!!!!!!!!!!!! Missed one"
+  end
+  
 end
 
 browser.close
