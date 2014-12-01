d925b5018c3f29a0e0845a0a6d9b74394928fba7
yoni <johnny.meetme@gmail.com>
2014-12-01 21:19:48 +0200

    app ready

--- Gemfile
+++ Gemfile
@@ -2,13 +2,14 @@ source 'https://rubygems.org'
 
 group :default do
   gem 'sinatra'
-  gem 'unicorn'
-  gem 'watir-webdriver'  
+  gem 'unicorn'  
   gem 'sequel'
   gem 'pg'
+  gem 'json'
 end
 
 group :development do
   gem 'dotenv'
   gem 'shotgun'
+  gem 'watir-webdriver'  
 end
--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
-require 'sinatra'
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default, :development)
+Dotenv.load if ENV['RACK_ENV'].downcase != 'production'
+
+require_relative 'models/init'
 
 class App < Sinatra::Base
 
@@ -7,5 +12,20 @@ class App < Sinatra::Base
 	  set :raise_errors,      development?
 	  set :logging,           true
 	end
+
+  get '/' do
+    @companies = Company.all.
+      inject([]){|mem, donor| mem << {:value => donor.id, :label => donor.name}}.
+      to_json
+
+    erb :index
+  end
+
+  get '/company/:id' do
+    company_id = params['id'].to_i
+    @company = Company[company_id]    
+
+    erb :company
+  end
 end
 
